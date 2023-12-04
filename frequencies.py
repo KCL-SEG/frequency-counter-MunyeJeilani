e45a40e8133a86ce4e072653c763e35836fd5ed9
Munye Jeilani <munye.jeilani@kcl.ac.uk>
2023-12-04 15:00:27 +0000

    Passes 4/4 tests.

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,10 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for an_element in items:
+        key = str(an_element)
+        frequencies[key] = frequencies.get(key, 0) + 1
     return frequencies
